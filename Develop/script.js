ec625b3b24bc0f2fc0ed50667a330154138f4654
Will <willkim0608@gmail.com>
2022-10-25 13:00:35 -0700

    Added confirmation of choices

--- Develop/script.js
+++ Develop/script.js
@@ -21,16 +21,36 @@ if (passLength < 8 || passLength > 128){
 var isUpper = window.confirm("Include uppercase?");
 document.getElementById(isUpper);
 
+if (isUpper){
+  isUpper = "Yes"
+} else {
+  isUpper = "No"
+}
+
 // Asks if lowercase included
 var isLower; window.confirm("Include lowercase?");
 document.getElementById(isLower);
+
+if (isLower){
+  isLower = "Yes"
+} else {
+  isLower = "No"
+}
 
 // Asks if special characters included
 var isSpecial; window.confirm("Include special characters?");
 document.getElementById(isSpecial);
 
+if (isSpecial){
+  isSpecial = "Yes"
+} else {
+  isSpecial = "No"
+}
 
- 
+// Confirmation of users choices
+window.confirm("Confirm your choices " +
+  "Length: " + passLength + "  Uppercase: " + isUpper + "  Lowercase:  " + isLower + "  Special characters: " + isSpecial)
+
 passwordText.value = password;
 
 }
