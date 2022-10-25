edc1fbd8a133c21bd870d4a6ee103a74747030c8
Will <willkim0608@gmail.com>
2022-10-25 11:19:46 -0700

    Added userChoice variable and window prompt

--- Develop/script.js
+++ Develop/script.js
@@ -3,12 +3,20 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  //var password = generatePassword();
+ // var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
+  
+  var userChoice = window.prompt("Enter length of password between 8 and 128");
+
+
+
+
+ 
   passwordText.value = password;
 
 }
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
