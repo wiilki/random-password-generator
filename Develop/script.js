54d4deefce7082a252bf55214337e3e79574f8dd
Will <willkim0608@gmail.com>
2022-10-25 10:40:03 -0700

    Changed couple divs to sections. Renamed btn class to button for readability

--- Develop/script.js
+++ Develop/script.js
@@ -3,7 +3,7 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  //var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
