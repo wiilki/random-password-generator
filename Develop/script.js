c1641e4b7657a06ace7010e5cb3be62ccab768e1
Will <willkim0608@gmail.com>
2022-10-26 12:00:25 -0700

    Added/edited comments and re-arranged for readability

--- Develop/script.js
+++ Develop/script.js
@@ -5,28 +5,30 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
 
   // Variables
-  var possibleChar = [];
-  var wrongNumMsg = "Invalid choice. Choose a number between 8-128";
-  var noCharMsg = "You must choose at least 1 character type. Try again!";
   var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
   var specialChar = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~"];
   var lowerLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
   var upperLetters = lowerLetters.map(element => {
     return element.toUpperCase();
   });
+  var passArray = [];
+  var possibleChar = [];
+  var wrongNumMsg = "Invalid choice. Choose a number between 8-128";
+  var noCharMsg = "You must choose at least 1 character type. Try again!";
 
+  // Existing code
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
-  // Prompts for password length
+  // Prompts for password length. '+' is prompting for a number
   var passLength = +window.prompt("Enter length of password between 8 and 128");
 
-  // While userinput is invalid, user will get following prompt
+  // While userinput is invalid, user will see wrongMsg
   while (isNaN(passLength) || passLength < 8 || passLength > 128) {
     passLength = +window.prompt(wrongNumMsg);
   }
 
-  // Asks for character type choices
+  // Asks for character type choices. Returns true or false value
   var isUpper = window.confirm("Include uppercase?");
   var isLower = window.confirm("Include lowercase?");
   var isNumber = window.confirm("Include numbers?");
@@ -41,7 +43,8 @@ function writePassword() {
   } else if (!isUpper && !isLower && !isSpecial && !isNumber) {
     window.alert(noCharMsg);
   }
-  // Pushes selected character types into one single array
+
+  // Pushes selected character types into one array possibleChar
   if (isUpper) {
     for (var i = 0; i < upperLetters.length; i++) {
       possibleChar.push(upperLetters[i]);
@@ -66,9 +69,7 @@ function writePassword() {
     }
   }
 
-  var passArray = [];
-
-  // Pushes random character to array for the length of passLength
+  // Pushes random character to passArray for the length of passLength
   for (var y = 0; y < passLength; y++) {
     var randomChar = possibleChar[Math.floor(Math.random() * possibleChar.length)];
     if (randomChar) {
@@ -85,7 +86,9 @@ function writePassword() {
   function generatePassword(passArray) {
     document.getElementById("password").textContent = passArray;
 
-}
+  }
+
+  // Existing code
   passwordText.value = password;
 
 }
