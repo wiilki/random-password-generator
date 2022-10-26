d3ae31095caa9d626ae49eb28f860e051bec135f
Will <willkim0608@gmail.com>
2022-10-25 23:28:11 -0700

    Added uppercase variable using existing lowercase variable

--- Develop/script.js
+++ Develop/script.js
@@ -9,6 +9,10 @@ function writePassword() {
   var specialChar = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~"];
   var number = [1, 2, 3, 4, 5, 6, 7, 8, 9];
   var lowerLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  var upperLetters = lowerLetters.map(element => {
+    return element.toUpperCase();
+  });
+
 
   // var password = generatePassword();
   var passwordText = document.querySelector("#password");
