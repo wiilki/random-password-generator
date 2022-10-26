70393f4911ae1a82546f2bebfdc318cfe22e6bf0
Will <willkim0608@gmail.com>
2022-10-26 16:23:06 -0700

    Fixed spaces in specialChar

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function writePassword() {
 
   // Variables
   var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-  var specialChar = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~"];
+  var specialChar = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~"];
   var lowerLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
   var upperLetters = lowerLetters.map(element => {
     return element.toUpperCase();
